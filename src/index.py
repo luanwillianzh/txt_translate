2ca2dc307e123fe21fd8329ff7549b0fe8130ccd
luanwillianzh <74968256+luanwillianzh@users.noreply.github.com>
2025-01-22 08:36:59 -0300

    Update index.py

--- src/index.py
+++ src/index.py
@@ -1,15 +1,18 @@
 import requests
 from fastapi import FastAPI
 from googletrans import Translator
+from random import choice
 
-t = Translator()
+
 app = FastAPI()
 
 @app.get("/")
 def index(url: str=""):
+  t = Translator()
+  ua = requests.get("https://gist.githubusercontent.com/pzb/b4b6f57144aea7827ae4/raw/cf847b76a142955b1410c8bcef3aabe221a63db1/user-agents.txt").text.split("\n")
   if url == "":
     return "Hello World!"
   else:
-    txtify = requests.get(f"https://txtify.luanwillianzh04.workers.dev/{url}").text
+    txtify = requests.get(f"https://txtify.luanwillianzh04.workers.dev/{url}", headers={"User-Agent": choice(ua)}).text
     trad = [ i.text for i in t.translate(txtify.split("\n\n"), dest="pt") ]
     return "\n\n".join(trad)
